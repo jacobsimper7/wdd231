57c6139972b2a9bf80bb6401ed3eed98ab2c693d
jacobsimper7 <jacobsimper777@gmail.com>
2025-07-11 13:11:19 -0700

    fixing the json

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -6,3 +6,29 @@ async function getMemberData() {
     const data = await response.json();
     displayMembers(data.members);
 }
+
+getMemberData();
+
+function displayMembers(members) {
+    members.forEach(member => {
+        let card = document.createElement('section');
+        let name = document.createElement('h2');
+        let logo = document.createElement('img');
+        let contact = document.createElement('p');
+        let email = document.createElement('p');
+
+        name.textContent = member.name;
+        logo.setAttribute('src', member.image);
+        logo.setAttribute('alt', `Logo of ${member.name}`);
+        logo.setAttribute('loading', 'lazy');
+        contact.textContent = `Contact: ${member.contact}`;
+        email.innerHTML = `Email: <a href="mailto:${member.email}">${member.email}</a>`;
+
+        card.appendChild(name);
+        card.appendChild(logo);
+        card.appendChild(contact);
+        card.appendChild(email);
+
+        cards.appendChild(card);
+    });
+}
