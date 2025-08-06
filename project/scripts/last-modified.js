aaec6bf5b01f3d9daab1019e6391c1bafffe3f06
jacobsimper7 <jacobsimper777@gmail.com>
2025-08-06 15:44:14 -0700

    site plan finished

---
+++ project/scripts/last-modified.js
@@ -0,0 +1,16 @@
+let oLastModif = new Date(document.lastModified);
+let currentyear = new Date().getFullYear();
+
+lastModified.innerHTML = `Last Modified: <span class="highlight">${new Intl.DateTimeFormat(
+    "en-US",
+    {
+     dateStyle: "full"
+    }
+).format(oLastModif)}</span>`;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const ts = document.getElementById("timestamp");
+    if (ts) {
+      ts.value = new Date().toISOString();
+    }
+  });
