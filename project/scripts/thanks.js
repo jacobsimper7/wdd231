2db83463072aff8de639f24935753ca00c42ae7b
jacobsimper7 <jacobsimper777@gmail.com>
2025-08-12 17:34:29 -0700

    fixed thank you and review pages. Need try/catch

--- project/scripts/thanks.js
+++ project/scripts/thanks.js
@@ -6,14 +6,9 @@ updateLastModified();
 
 const params = new URLSearchParams(window.location.search);
 
-document.querySelector("#result").innerHTML = `
-    <h2>Confirmation Details</h2>
-    <p><strong>Name:</strong> ${params.get("first") || ""} ${params.get("last") || ""}</p>
-    <p><strong>Organizational Title:</strong> ${params.get("orgtitle") || ""}</p>
-    <p><strong>Email:</strong> ${params.get("email") || ""}</p>
-    <p><strong>Phone:</strong> ${params.get("phone") || ""}</p>
-    <p><strong>Business/Organization Name:</strong> ${params.get("organization") || ""}</p>
-    <p><strong>Membership Level:</strong> ${params.get("membership") || ""}</p>
-    <p><strong>Description:</strong> ${params.get("description") || ""}</p>
-    <p><strong>Form Submitted:</strong> ${formattedDate}</p>
-`;
+let html = "<h2>Order Confirmation</h2>";
+for (const [key, value] of params.entries()) {
+  html += `<p><strong>${key.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase())}:</strong> ${decodeURIComponent(value)}</p>`;
+}
+
+document.getElementById("result").innerHTML = html;
